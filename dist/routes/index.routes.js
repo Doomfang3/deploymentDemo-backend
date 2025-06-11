89b78d26be1da393eab2e438902df39195be8ec3
Doomfang3 <saubinmathieu@gmail.com>
2025-06-11 09:31:51 +0200

    Revert "Remove postbuild to add postinstall and copy of prisma client after build"
    This reverts commit 5faf93065310bfa7933ca4419f450831f7d6cfc3.

--- dist/routes/index.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const router = require('express').Router();
-router.get('/', (req, res, next) => {
-    res.json('All good in here');
-});
-const bookRoutes = require('./book.routes');
-router.use('/books', bookRoutes);
-module.exports = router;
